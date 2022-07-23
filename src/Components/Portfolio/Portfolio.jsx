27a6eb1c754be118abba9b0420b6049dd11d5581
Asilbek1111 <99465065+Asilbek1111@users.noreply.github.com>
2022-07-23 22:59:48 +0300

    chsnge rasm

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,39 +2,45 @@ import React from "react";
 import "./Portfolio.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import Sidebar from "../../img/sidebar.png";
-import Ecommerce from "../../img/ecommerce.png";
-import Hoc from "../../img/hoc.png";
-import MusicApp from "../../img/musicapp.png";
+import Sidebar from "../../img/youtube.png";
+import Ecommerce from "../../img/movies.webp";
+import Hoc from "../../img/bootstrap.png";
+import MusicApp from "../../img/dog.png";
 import { themeContext } from "../../Context";
 import { useContext } from "react";
 
 const Portfolio = () => {
-    const theme = useContext(themeContext);
-    const darkMode = theme.state.darkMode;
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
     <div className="portfolio" id="Portfolio">
       {/*Heading*/}
-      <span style={{color: darkMode? 'white':''}}>Recent Projects</span>
+      <span style={{ color: darkMode ? "white" : "" }}>Recent Projects</span>
       <span>Portfolio</span>
 
       {/*Slider*/}
       <Swiper
-      spaceBetween={-200}
-      slidesPerView={3}
-      grabCursor={true}
-      className='portfolio-slider'
+        spaceBetween={-200}
+        slidesPerView={3}
+        grabCursor={true}
+        className="portfolio-slider"
       >
         <SwiperSlide>
-          <img src={Sidebar} alt="" />
+          <a href="https://utubedownload.netlify.app/" target="_blank">
+            <img src={Sidebar} alt="" />
+          </a>
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src={Ecommerce} alt="" />
+          <a href="https://moviee-search-js.netlify.app/" target="_blank">
+            <img src={Ecommerce} alt="" />
+          </a>
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src={MusicApp} alt="" />
+          <a href="http://aslbekblog.epizy.com/">
+            <img src={MusicApp} alt="" />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
           <img src={Hoc} alt="" />
