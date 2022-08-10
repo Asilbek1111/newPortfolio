06a8c88c8842e0aba55c3cb4acd67f7c543268bd
Asilbek1111 <99465065+Asilbek1111@users.noreply.github.com>
2022-08-10 10:28:44 +0300

    Experience

--- src/Components/Experince/Experince.jsx
+++ src/Components/Experince/Experince.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import './Experince.scss'
-
+import React from "react";
+import "./Experince.scss";
 
 const Experince = () => {
-    return (
-      <div className="experience" id='Experience'> 
-        <div className="achievement">
-          <div className="circle">1.5+</div>
-          <span>years </span>
-          <span>Experience</span>
-        </div>
-        <div className="achievement">
-          <div className="circle">15+</div>
-          <span>completed </span>
-          <span>Projects</span>
-        </div>
-        <div className="achievement">
-          <div className="circle">1</div>
-          <span>companies</span>
-          <span>Worked</span>
-        </div>
-
+  return (
+    <div className="experience" id="Experience">
+      <div className="achievement">
+        <div className="circle">1.5+</div>
+        <span>years </span>
+        <span>Experience</span>
       </div>
-    );
-}
+      <div className="achievement">
+        <div className="circle">10+</div>
+        <span>completed </span>
+        <span>Projects</span>
+      </div>
+      <div className="achievement">
+        <div className="circle">0</div>
+        <span>companies</span>
+        <span>Worked</span>
+      </div>
+    </div>
+  );
+};
 
 export default Experince;
